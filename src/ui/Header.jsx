c34dec7395068023fe050870223053d4bebd439d
Sodiq Atiku <hatykuxordik@gmail.com>
2023-11-19 11:35:30 +0100

    Initial Commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>Sodiq</p>
+    </div>
+  );
+}
+
+export default Header;
